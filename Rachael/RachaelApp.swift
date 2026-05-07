7546197cec9c42085cd873ec63de64cc31b9e546
J1 <gjeus0707@gmail.com>
2026-05-08 01:12:11 +0900

    Initial Commit

---
+++ Rachael/RachaelApp.swift
@@ -0,0 +1,17 @@
+//
+//  RachaelApp.swift
+//  Rachael
+//
+//  Created by Jaewon Kim on 5/8/26.
+//
+
+import SwiftUI
+
+@main
+struct RachaelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
